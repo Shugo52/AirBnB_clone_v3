7e4e7c6b8017dff53ffe07e7b7e2049d8b4dd80d
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-27 12:42:45 +0100

    404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Flask app Module"""
 
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close(exception):
     storage.close
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
