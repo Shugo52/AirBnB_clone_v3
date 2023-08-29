3f2a277801284c9ed35d977b468682e384564057
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-29 04:11:42 +0100

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """index page for flask app"""
 
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
 from api.v1.views import app_views
 
@@ -11,15 +11,18 @@ def getstatus():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def getcount():
-    count_dict = {"amenities": 'Amenity',
-                  "cities": 'City',
-                  "places": 'Place',
-                  "reviews": 'Review',
-                  "states": 'State',
-                  "users": 'User'}
-
-    for k in count_dict.keys():
-        count_dict[k] = storage.count(count_dict.get(k))
-    return jsonify(count_dict)
+    if request.method == 'GET':
+        response = {}
+        models = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in models.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
