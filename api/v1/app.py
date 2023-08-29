7584a9e66e8f3982b411e27ddf4b1abb77954174
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-29 08:13:07 +0100

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Flask app Module"""
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -18,7 +18,7 @@ def close(exception):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
