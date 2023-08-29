783de08ee0f077916ebff02bbb51c5a194adabd4
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-29 08:15:14 +0100

    Update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,6 +3,7 @@
 
 from models import storage
 from models.city import City
+from models.state import State
 from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
 
